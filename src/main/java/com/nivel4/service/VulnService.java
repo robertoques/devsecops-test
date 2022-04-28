aaae59facf4b41d8c8f3cf2898e8ccc8c565d8e8
r0b3 <robertoques1@gmail.com>
2022-04-28 16:52:04 -0400

    New vuln method

--- src/main/java/com/nivel4/service/VulnService.java
+++ src/main/java/com/nivel4/service/VulnService.java
@@ -2,9 +2,20 @@ package com.nivel4.service;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.io.FileOutputStream;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.Charset;
 
 public class VulnService {
-    
+   
+       protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+  String json = "{\"key\":\""+req.getParameter("value")+"\"}";
+  FileOutputStream fos = new FileOutputStream("output.json");
+  fos.write(json.getBytes(Charset.forName("UTF-8")));  // Noncompliant
+}
 
     public void CommandInjectionVulnerability(String path) throws SQLException, IOException {
 
