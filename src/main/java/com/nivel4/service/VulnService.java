495c78d54cf580a45ae18f01db3c969248a16def
r0b3 <robertoques1@gmail.com>
2022-04-29 09:39:52 -0400

    Add some vulns

--- src/main/java/com/nivel4/service/VulnService.java
+++ src/main/java/com/nivel4/service/VulnService.java
@@ -2,6 +2,7 @@ package com.nivel4.service;
 
 import java.io.IOException;
 import java.sql.SQLException;
+
 import java.io.FileOutputStream;
 import javax.servlet.http.*;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.Charset;
 
+import java.util.Random;
+
+
+
 public class VulnService {
+
+    public static void main( String args[] ) {
+      Random rand = new Random(); //instance of random class
+      int upperbound = 25;
+        //generate random values from 0-24
+      int int_random = rand.nextInt(upperbound); 
+      double double_random=rand.nextDouble();
+      float float_random=rand.nextFloat();
+      
+      System.out.println("Random integer value from 0 to" + (upperbound-1) + " : "+ int_random);
+      System.out.println("Random float value between 0.0 and 1.0 : "+float_random);
+      System.out.println("Random double value between 0.0 and 1.0 : "+double_random);
+    }
+
+
    
        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
   String json = "{\"key\":\""+req.getParameter("value")+"\"}";
